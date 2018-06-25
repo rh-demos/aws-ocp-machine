5a014e6181b826d82b522c1c7678150aee85f697
Jacob Borella <jacobborella@hotmail.com>
2018-06-26 00:09:25 +0200

    added ansible service broker

--- roles/ocp/files/install.sh
+++ roles/ocp/files/install.sh
@@ -1,2 +1,3 @@
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+sudo subscription-manager unregister
